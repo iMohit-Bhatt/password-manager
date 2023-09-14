c168e02c1c8d187a0751722b89944bccb0a64663
iMohit-Bhatt <mohit.bhatt.work@gmail.com>
2023-09-14 11:56:25 +0530

    Generate Password feature added

--- main.py
+++ main.py
@@ -6,29 +6,39 @@ import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
-
-password_list = []
-
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
-
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
-
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
-
-random.shuffle(password_list)
-
 password = ""
-for char in password_list:
-  password += char
 
-print(f"Your password is: {password}")
+def generate():
+  nr_letters = random.randint(8, 10)
+  nr_symbols = random.randint(2, 4)
+  nr_numbers = random.randint(2, 4)
+
+  password_list = []
+
+  for char in range(nr_letters):
+    password_list.append(random.choice(letters))
+
+  for char in range(nr_symbols):
+    password_list += random.choice(symbols)
+
+  for char in range(nr_numbers):
+    password_list += random.choice(numbers)
+
+  random.shuffle(password_list)
+
+  password = ""
+  for char in password_list:
+    password += char
+
+  password_entry.delete(0, END)
+  password_entry.insert(0, password)
+
+  window.clipboard_clear()
+  window.clipboard_append(password)
+
+  messagebox.showinfo(title="website", message="password copied to clipboard")
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add():
     website = website_entry.get()
@@ -45,7 +55,7 @@ def add():
                 datafile.write(f'{website} | {email} | {password}\n')
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
-
+   
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
